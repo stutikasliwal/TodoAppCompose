cf3eed00e05e12be7abae933c7ea71fc1d461523
stuti <stuti@thoughtwin.com>
2024-02-01 12:31:19 +0530

    Add space change 2

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/MainActivity.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/MainActivity.kt
@@ -23,7 +23,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
             CleanArchitectureNoteAppTheme {
                 Surface(color = MaterialTheme.colors.background) {
